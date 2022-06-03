eb0a9bb6e9d91beaa81d2301f92fcea855db98a2
PinedaVictor <pinedavictor095@gmail.com>
2022-06-03 16:58:27 -0700

    Handling api route server response

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,44 @@ const APIRouter = Router();
 
 APIRouter.get("/ping", (req, res) => {
   res.json({ success: true });
+  res.status(200);
+  return;
 });
+
 APIRouter.get("/posts", (req, res) => {
-  res.json({ postData: "YASS" });
+  const requestParameters = req.query;
+
+  //   checking for valid query paremeters
+  if (!requestParameters["tags"]) {
+    res.json({ error: "tags parameter is required" });
+    res.status(400);
+    return;
+  }
+
+  if (
+    requestParameters["sortBy"] &&
+    !requestParameters["sortBy"].match(/^(id|reads|likes|popularity)$/)
+  ) {
+    res.json({ error: "sortBy parameter is required" });
+    res.status(400);
+    return;
+  }
+
+  if (
+    requestParameters["direction"] &&
+    !requestParameters["direction"].match(/^(desc|asc)$/)
+  ) {
+    res.json({ error: "direction parameter is required" });
+    res.status(400);
+    return;
+  }
+
+  console.log(requestParameters);
+  //   parameter defualts: sortBy: id, direction: asc
+
+  res.send("SUP");
+
+  //   We must now check for valid sortBy and direction parameters
 });
 
 export default APIRouter;
