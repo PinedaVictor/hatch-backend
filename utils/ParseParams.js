cf10c322309809f0f1831aff649bf79b92c8fba1
PinedaVictor <pinedavictor095@gmail.com>
2022-06-06 11:36:50 -0700

    Cleaned up ParseParams function

--- utils/ParseParams.js
+++ utils/ParseParams.js
@@ -9,24 +9,20 @@ export const parseParameters = (inputParams) => {
       sortBy: inputParams["sortBy"],
       direction: inputParams["direction"],
     };
-    return query;
   } else if (inputParams["sortBy"]) {
     query = {
       tags: tags,
       sortBy: inputParams["sortBy"],
       direction: defualts["direction"],
     };
-    return query;
   } else if (inputParams["direction"]) {
     query = {
       tags: tags,
       sortBy: defualts["sortBy"],
       direction: inputParams["direction"],
     };
-
-    return query;
   } else {
     query = { tags: tags, ...defualts };
-    return query;
   }
+  return query;
 };
